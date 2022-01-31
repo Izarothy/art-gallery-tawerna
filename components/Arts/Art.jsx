45f77718dd4e72d297d7ad85f88494a699be1e52
Izarothy <izarothmail@gmail.com>
2022-01-31 19:31:40 +0100

    feat: open image in new tab on click

--- components/Arts/Art.jsx
+++ components/Arts/Art.jsx
@@ -5,15 +5,17 @@ const Art = ({ url, name }) => {
   return (
     <div className="flex flex-col ">
       <div>
-        <Image
-          src={url}
-          width={256}
-          height={256}
-          layout="responsive"
-          alt="Art"
-          className="rounded-xl hover:opacity-40 cursor-pointer"
-          objectFit="cover"
-        />
+        <a href={url} target="_blank" rel="noreferrer">
+          <Image
+            src={url}
+            width={256}
+            height={256}
+            layout="responsive"
+            alt="Art"
+            className="rounded-xl hover:opacity-40 cursor-pointer"
+            objectFit="cover"
+          />
+        </a>
       </div>
       <p className="text-center py-2">{name}</p>
     </div>
